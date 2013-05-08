d7d83c0c7c85057d433f916513ec8d43b12afe6c
Ruben Orta <ruben.orta@gmail.com>
2013-05-08 22:31:11 +0200

    Counter fix

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -2,11 +2,11 @@ import sys
 import json
 import operator
 from collections import defaultdict
+from collections import Counter
 
 def process_tweets(file):
-
+    lista = []
     ranking = defaultdict(int)
-
     for line in file:
     	tweet = json.loads(line)
     	if not 'delete' in tweet:
@@ -14,15 +14,19 @@ def process_tweets(file):
           if 'hashtags' in entities:
               if entities['hashtags']:
               	  for hashtag in entities['hashtags']:
-                      text = hashtag['text'].lower()
-                      ranking[text] +=1
-    sort_ranking = sorted(ranking.iteritems(), key= lambda x: x[1], reverse=True) 
+                      #text = hashtag['text'].lower()
+                      text = hashtag['text']
+                      lista.append(hashtag['text'])
+                      #ranking[text] += 1.0
+    c = Counter(lista)
+    top_tags = sorted(c.items(), key = lambda x: x[1], reverse=True)
+    #sort_ranking = sorted(ranking.iteritems(), key= lambda x: x[1], reverse=True) 
     for pos in range(0,10):
-        #output = "{0}\t{1}" .format(sort_ranking[pos][0],sort_ranking[pos][1])
-        #print output.encode('utf-8')
-        word = sort_ranking[pos][0]
-        number = float(sort_ranking[pos][1])
-        print "%s\t%s" % (word,str(number))
+        output = "{0}\t{1}" .format(top_tags[pos][0],top_tags[pos][1])
+        print output.encode('utf-8')
+        #word = sort_ranking[pos][0]
+        #number = float(sort_ranking[pos][1])
+        #print "%s\t%s" % (word,str(number))
 
 def main():
     tweet_file = open(sys.argv[1])
